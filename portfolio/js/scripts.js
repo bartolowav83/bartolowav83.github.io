c173ccdd0c8608d5c5d7522cdf0c5b65ae25fea0
bartolowav83 <bartolowav@gmail.com>
2024-12-01 10:18:57 +0100

    Add files via upload

---
+++ portfolio/js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Merci de m'avoir contacté ! Je vous répondrai bientôt.");
+});

f6d98763821824aa74c5600fdb370f3409154008
bartolowav83 <bartolowav@gmail.com>
2024-12-01 10:44:40 +0100

    Delete portfolio/js directory

--- portfolio/js/scripts.js
+++
@@ -1,6 +0,0 @@
-// scripts.js
-
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Merci de m'avoir contacté ! Je vous répondrai bientôt.");
-});
